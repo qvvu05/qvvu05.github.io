8964711c00867948b6f6d34a82f7100988fc33e6
qvvu05 <37234174+qvvu05@users.noreply.github.com>
2018-03-30 14:40:48 +0700

    finish table

--- table/js/main.js
+++ table/js/main.js
@@ -1,62 +1,101 @@
-var data=[
+var data = [
     {
-        picture: "<img src='img/p1.png'></img>",
-        name: 'Ann',
+        picture: "<img src='img/SonGoku.jpg'></img>",
+        name: 'Son Goku',
         gender: 'male',
-        email: 'abc@123.com',
+        email: 'anhanhsuotbophim@123.com',
         phone: 1
     },
     {
-        picture: "<img src='img/p1.png'></img>",
-        name: 'Charlie',
+        picture: "<img src='img/Bulma.jpg'></img>",
+        name: 'Bulma',
         gender: 'female',
-        email: 'abc@123.com',
+        email: 'kieunusexy@123.com',
         phone: 2
     },
     {
-        picture: "<img src='img/p1.png'></img>",
-        name: 'Bob',
+        picture: "<img src='img/Kame.jpg'></img>",
+        name: 'Quy lão Kame',
         gender: 'male',
-        email: 'abc@123.com',
+        email: 'thichgaidep@gmail.com',
         phone: 3
     },
     {
-        picture: "<img src='img/p1.png'></img>",
-        name: 'Alex',
-        gender: 'male',
-        email: 'abc@123.com',
+        picture: "<img src='img/zamas.jpg'></img>",
+        name: 'Zamas tương lai',
+        gender: 'other',
+        email: 'fakegoku@gmail.com',
         phone: 4
-    }
+    },
+    {
+        picture: "<img src='img/beerus.jpg'></img>",
+        name: 'Beerus Sama',
+        gender: 'male',
+        email: 'sieunhatvutru@123.com',
+        phone: 3
+    },
+    {
+        picture: "<img src='img/vegeta.jpg'></img>",
+        name: 'Vegeta',
+        gender: 'male',
+        email: 'deptrainhathanhtinh@123.com',
+        phone: 3
+    },
+    {
+        picture: "<img src='img/number18.jpg'></img>",
+        name: '18',
+        gender: 'male',
+        email: 'sexy@gmail.com',
+        phone: 3
+    },
 ];
 
 /**
- * sort before show data
- * click-> getvalue to be sort
- * sort
- * show data
+ * this code will look at current direction of sort and inverse it
  */
+var nameDirection = 1;
+var genderDirection = 1;
+var emailDirection = 1;
+var phoneDirection = 1;
 
+var property;
+var newData = [];
+function getVal(btn) {
+    property = btn.value;
+    // alert(btn.value);
+    switch (property) {
+        case 'Name': {
 
-/************************************************************************** */
-function dynamicSort(property){
-    return function(a,b) {
-        return a[property]>b[property];
+            if (nameDirection == 1) { newData = data.sort(function (a, b) { return (a.name > b.name) }); break; } else { newData = data.sort(function (a, b) { return (a.name < b.name) }); break; }
+        }
+
+        case 'Gender': {
+            if (genderDirection == 1) { newData = data.sort(function (a, b) { return (a.gender > b.gender) }); break; } else { newData = data.sort(function (a, b) { return (a.gender < b.gender) }); break; }
+        }
+        case 'Email': {
+            if (emailDirection == 1) { newData = data.sort(function (a, b) { return (a.email > b.email) }); break; } else { newData = data.sort(function (a, b) { return (a.email < b.email) }); break; }
+        }
+        case 'Phone': {
+            if (phoneDirection == 1) { newData = data.sort(function (a, b) { return (a.phone > b.phone) }); break; } else { newData = data.sort(function (a, b) { return (a.phone < b.phone) }); break; }
+        }
+        default:
+            break;
+    }
+
+}
+
+function showData() {
+    $('tbody').html('');//reset data
+    for (var i = 0; i < newData.length; i++) {
+        $('tbody').append('<tr><td>' + newData[i].picture + '</td><td>' +
+            newData[i].name + '</td><td>' + newData[i].gender +
+            '</td><td>' + newData[i].email + '</td><td>' + newData[i].phone + '</td></tr>');
     }
 }
-//get property here and pass into dynamicSort
-var newData=data.sort(dynamicSort(''));
-// var newData=data.sort(function(a,b){return a.name>b.name});
 
-/************************************************************************** */
-
-for(var i=0; i< data.length;i++){
-    // $('tbody').append('<tr><td>' + data[i].picture + '</td><td>' +
-    //     data[i].name + '</td><td>' + data[i].gender +
-    //     '</td><td>' + data[i].email + '</td><td>' + data[i].phone + '</td></tr>');
-    
-    $('tbody').append('<tr><td>' + newData[i].picture + '</td><td>' +
-        newData[i].name + '</td><td>' + newData[i].gender +
-        '</td><td>' + newData[i].email + '</td><td>' + newData[i].phone + '</td></tr>');
-
-}
-
+//start the program: show the original data
+for (var i = 0; i < data.length; i++) {
+    $('tbody').append('<tr><td>' + data[i].picture + '</td><td>' +
+        data[i].name + '</td><td>' + data[i].gender +
+        '</td><td>' + data[i].email + '</td><td>' + data[i].phone + '</td></tr>');
+} 
