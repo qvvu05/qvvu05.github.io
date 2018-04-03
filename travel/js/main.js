c393ed458ccef117cd59906ad5eb7950b39e66b3
Quyen <qvvu05@gmail.com>
2018-04-04 01:09:43 +0700

    finish travel project

--- travel/js/main.js
+++ travel/js/main.js
@@ -1,2 +1,23 @@
 // $(document).ready(function);
 $(".dropdown-trigger").dropdown({ hover: false });
+
+/**
+ * if offsetTop of .aside > offsetTop of main-content: aside will have 
+ * an addition class: hide with property: display:none!important;
+ */
+
+window.onscroll = function () { myFunction() };
+
+var asideBar = document.getElementById("asideBar");
+var topOfAsideBar = asideBar.offsetTop;
+
+
+function myFunction() {
+    console.log('asideBar: ' + topOfAsideBar);
+    console.log('windows: '+ window.pageYOffset);
+    if (window.pageYOffset >= 950) {
+        asideBar.classList.add("sticky");
+    } else {
+        asideBar.classList.remove("sticky");
+    }
+}
