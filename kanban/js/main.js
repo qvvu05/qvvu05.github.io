c178dfc79f85f2e2eef3a6bf86e0cef25c59e4e6
qvvu05 <37234174+qvvu05@users.noreply.github.com>
2018-04-06 12:38:38 +0700

    [kanban] adding function:create job

--- kanban/js/main.js
+++ kanban/js/main.js
@@ -1,3 +1,26 @@
+var app={
+    /**adding jobs, args:  */
+    newJob:function (type,input) {
+        let event=window.event;
+        let jobName=$('input').val();
+
+        if (event.keyCode===13 && jobName.trim()!=='') {
+            //add new job
+            this.addJobToList(type,jobName);
+            //reset input for user can adding anotherjob
+            $('input').val('');
+        }
+    },
+
+    addJobToList: function (type, jobName){
+        var content = '<a href="#!" class="collection-item">'+ jobName + '</a >'
+        $('#'+type).append(content);
+    }
+
+}
+
+
+
 // sort inside each col-jqueryui-sortable()
 $(function () {
     $(".connectedSortable").sortable({
